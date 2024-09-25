c51223e0f6039730e3ee30af59442f306012d8bf
paodealho404 <phbn@ic.ufal.br>
2024-09-24 23:38:44 -0300

    feat: :sparkles: implementa funções úteis para o SPI

--- main.c
+++ main.c
@@ -3,60 +3,154 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include "hps_0.h"
-//settings for the lightweight HPS-to-FPGA bridge
-#define HW_REGS_BASE ( ALT_STM_OFST )
-#define HW_REGS_SPAN ( 0x04000000 ) //64 MB with 32 bit adress space this is 256 MB
-#define HW_REGS_MASK ( HW_REGS_SPAN - 1 )
- 
- 
-//setting for the HPS2FPGA AXI Bridge
+#include "hwlib.h"
+#include "soc_cv_av/socal/socal.h"
+#include "soc_cv_av/socal/hps.h"
+#include "soc_cv_av/socal/alt_gpio.h"
+#include "errno.h"
+#include <unistd.h>
+
+// settings for the lightweight HPS-to-FPGA bridge
+#define HW_REGS_BASE (ALT_STM_OFST)
+#define HW_REGS_SPAN (0x04000000) // 64 MB with 32 bit adress space this is 256 MB
+#define HW_REGS_MASK (HW_REGS_SPAN - 1)
+
+// setting for the HPS2FPGA AXI Bridge
 #define ALT_AXI_FPGASLVS_OFST (0xC0000000) // axi_master
-#define HW_FPGA_AXI_SPAN (0x40000000) // Bridge span 1GB
-#define HW_FPGA_AXI_MASK ( HW_FPGA_AXI_SPAN - 1 )
+#define HW_FPGA_AXI_SPAN (0x40000000)	   // Bridge span 1GB
+#define HW_FPGA_AXI_MASK (HW_FPGA_AXI_SPAN - 1)
 
-static const struct spi {
-    void *sck_addr;
-    void *ss_addr;
-    void *mosi_addr;
-    void *miso_addr;
-} spi_t;
+#define SPI_SCK_DIV 8
+#define SPI_COMM_PERIOD 1/(SPI_SCK_FREQ>>SPI_SCK_DIV)
 
-int setup_mem_addr(){
-    
+static struct spi
+{
+	uint32_t *sck_addr;
+	uint32_t *ss_addr;
+	uint32_t *mosi_addr;
+	uint32_t *miso_addr;
+} spi_fields = {
+	.sck_addr = NULL,
+	.ss_addr = NULL,
+	.mosi_addr = NULL,
+	.miso_addr = NULL};
+
+int setup_mem_addr(struct spi *spi)
+{
+
 	void *virtual_base;
+	void *axi_virtual_base;
 	int fd;
 
 	// map the address space for the LED registers into user space so we can interact with them.
 	// we'll actually map in the entire CSR span of the HPS since we want to access various registers within that span
 
-	if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) {
-		printf( "ERROR: could not open \"/dev/mem\"...\n" );
-		return( 1 );
+	if ((fd = open("/dev/mem", (O_RDWR | O_SYNC))) == -1)
+	{
+		printf("ERROR: could not open \"/dev/mem\"...\n");
+		return -EIO;
 	}
 
-	virtual_base = mmap( NULL, HW_REGS_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, HW_REGS_BASE );
+	virtual_base = mmap(NULL, HW_REGS_SPAN, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, HW_REGS_BASE);
 
-	if( virtual_base == MAP_FAILED ) {
-		printf( "ERROR: mmap() failed...\n" );
-		close( fd );
-		return( 1 );
+	if (virtual_base == MAP_FAILED)
+	{
+		printf("ERROR: mmap() failed...\n");
+		close(fd);
+		return -EFAULT;
 	}
-	
-	h2p_lw_led_addr=virtual_base + ( ( unsigned long  )( ALT_LWFPGASLVS_OFST + PIO_LED_BASE ) & ( unsigned long)( HW_REGS_MASK ) );
-	
 
-    return 0;
+	axi_virtual_base = mmap(NULL, HW_FPGA_AXI_SPAN, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, ALT_AXI_FPGASLVS_OFST);
+
+	if (axi_virtual_base == MAP_FAILED)
+	{
+		printf("ERROR: axi mmap() failed...\n");
+		close(fd);
+		return -EFAULT;
+	}
+
+	spi->miso_addr = axi_virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + SPI_MISO_BASE) & (unsigned long)(HW_REGS_MASK));
+	spi->mosi_addr = axi_virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + SPI_MOSI_BASE) & (unsigned long)(HW_REGS_MASK));
+	spi->sck_addr = axi_virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + SPI_SCK_BASE) & (unsigned long)(HW_REGS_MASK));
+	spi->ss_addr = axi_virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + SPI_SS_BASE) & (unsigned long)(HW_REGS_MASK));
+
+	*(spi->miso_addr) = 0x0;
+	*(spi->mosi_addr) = 0x0;
+	*(spi->sck_addr) = 0x0;
+	*(spi->ss_addr) = 0x1;
+	
+	return 0;
 }
 
-int main() {
-    printf("Initializing data transfer!\n");
-    uint32_t *sck_addr = 
+// void send_header();
+// enum colors {
+// 	C_RED = 0,
 
-    while (1) {
-        IOWR_ALTERA_AVALON_PIO_DATA(SPI_SS_BASE, 0);
-        IOWR_ALTERA_AVALON_PIO_DATA(SPI_SCK_BASE, 0);
-        IOWR_ALTERA_AVALON_PIO_DATA(SPI_SS_BASE, 1);
-        IOWR_ALTERA_AVALON_PIO_DATA(SPI_SCK_BASE, 1);
-    }
-    return 0;
+// } color;
+
+// void serialize_channel(enum colors);
+
+// void result();
+static inline void set_mosi(struct spi *spi)
+{
+	*(spi->mosi_addr) = 0x1;
+}
+
+static inline void clear_mosi(struct spi *spi)
+{
+	*(spi->mosi_addr) = 0x0;
+}
+
+static inline void toggle_sck(struct spi *spi)
+{
+	*(spi->sck_addr) ^= 0x1;
+}
+
+static inline void set_ss(struct spi *spi)
+{
+	*(spi->ss_addr) = 0x1;
+}
+
+static inline void clear_ss(struct spi *spi)
+{
+	*(spi->ss_addr) = 0x0;
+}
+
+int main()
+{
+	printf("Initializing data transfer!\n");
+	int err = 0;
+
+	err = setup_mem_addr(&spi_fields);
+	if (err)
+	{
+		printf("Error setting up memory addresses\n");
+		return err;
+	}
+
+	printf("SPI Initialized!\n");
+	uint8_t count = 0;
+	while (1)
+	{
+		printf("Looping...\n");
+
+		if(count == 0xFF){
+			break;
+		}
+
+		set_ss(&spi_fields);
+
+		toggle_sck(&spi_fields);
+
+		if (*(spi_fields.sck_addr) == 0) {
+			continue;
+		}
+		
+		set_mosi(&spi_fields);
+
+		usleep(SPI_COMM_PERIOD * 1000000 * 10);
+		count +=1;
+		
+	}
+	return 0;
 }
